7771efa6184a6c7deb5e98e201f818148d19d75a
Mark Allison <mark@stylingandroid.com>
2020-10-03 15:17:59 +0100

    Code for Fade article

--- app/src/main/java/com/stylingandroid/mdcmotion/ui/AppearFragment.kt
+++ app/src/main/java/com/stylingandroid/mdcmotion/ui/AppearFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.transition.TransitionManager
+import com.google.android.material.transition.MaterialFade
 import com.google.android.material.transition.MaterialFadeThrough
+import com.stylingandroid.mdcmotion.R
 import com.stylingandroid.mdcmotion.databinding.FragmentAppearBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,6 +30,17 @@ class AppearFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAppearBinding.inflate(inflater, container, false)
+        binding.showButton.setOnClickListener {
+            TransitionManager.beginDelayedTransition(binding.root, MaterialFade())
+            val resources = layoutInflater.context.resources
+            if (binding.showButton.text == resources.getString(R.string.show)) {
+                binding.showButton.text = resources.getString(R.string.hide)
+                binding.motionImage.visibility = View.VISIBLE
+            } else {
+                binding.showButton.text = resources.getString(R.string.show)
+                binding.motionImage.visibility = View.INVISIBLE
+            }
+        }
         return binding.root
     }
 }
